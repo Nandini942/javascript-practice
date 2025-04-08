863ff6b52ab777e48ef216958ce6b559a6e56aef
Nandini Sethi <89060909+Nandini942@users.noreply.github.com>
2025-04-08 18:47:37 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,77 @@
+// Two types of datatype :
+// Primitive datatype- call by value
+// 1. String
+// const str= "Nandini"
+// console.log(str)
+// console.log(typeof str);
+
+// // 2. Number
+// const numValue=20
+// console.log(numValue);
+// console.log(typeof numValue);
+
+
+// // 3. Boolean
+// const isLoggedIn=true
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+
+
+// // 4. Null- Empty
+// const Temperature=null
+// console.log(Temperature);
+// console.log(typeof Temperature);
+
+
+// // 5. undefined
+// let email;
+// console.log(email);
+// console.log(typeof email);
+
+
+// 6. Symbol- to make any value unique
+// const anyNumber= Symbol('123')
+// const anotherNumber=Symbol('123')
+// console.log(anyNumber===anotherNumber);
+
+// 7. BigInt
+// let bigNumber=1532738628932902n
+// console.log(bigNumber);
+// console.log(typeof bigNumber);
+
+
+// Reference or Non primtive datatype
+// 1. Array
+// const actors=["Sharukh Khan", "Sidharth Malhotra", "Shahid Kapoor"]
+
+// // 2. Objects
+
+//     let my_obj={
+//         first_name:"Nandini",
+//         last_name:"Sethi",
+//         age:23,
+//     }
+
+// // 3. Functions
+const myFunction = function(){
+    console.log("Hello World")
+}
+console.log(typeof myFunction);
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
+
+
