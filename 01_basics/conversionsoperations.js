9bfde166635af9254171734864df3b348e511bd9
Nandini Sethi <89060909+Nandini942@users.noreply.github.com>
2025-04-08 17:40:13 +0000

    commit datatypes

---
+++ 01_basics/conversionsoperations.js
@@ -0,0 +1,33 @@
+// // let score="50"
+// let score= "50abl"
+// let anyData=null;
+// console.log(typeof(anyData)); 
+// // Datatype of the anyData is Object
+// let otherData=Number(anyData)
+// console.log(otherData)
+// // when trying to convert null value into number it gives output zero
+// console.log(typeof score);
+// // When assigned value of score the datatype is showing as number but it is actually not a number
+// //it gives output as sring
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// // In javascript the value can be converted to any datatype but it is not necessary that the value has exactly been changed\
+// // for example above the value of score variable is 50abl which is not a number but the is typecasted and changed the type of the variable from string to number it is changed to Number
+// // But the value inside the variable valueInNumber is NaN
+let number= true
+console.log(typeof number);
+// Data type of the number is boolean
+let second_number= Number(number)
+// when converted into Number datatype it gets converted
+console.log(typeof second_number);
+console.log(second_number)
+//This outputs 1
+
+
+
+
+
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; 
+// treat all javascript as the newer version
+// alert(4+4) //we are using node js
+console.log(4+4)
+console.log("Nandini") //code readabilty should be high
+
+// primitive datatypes
+/* number=> 2 power to 53
+big int
+string- within double quotes
+boolean- true/false
+null- standalone value- variable has no value or empty value
+undefined- variable is declared but value is not assigned
+symbol- uniqueness 
+*/
+
+//object
+let accountName= "String"
+let accountID=null
+let accountCity
+console.log(typeof accountName );
+console.log(typeof accountID)
+// typeof null is object
+console.log(typeof accountCity);
+//typeof undefined is undefined
+
+
